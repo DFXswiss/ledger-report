24c188bc5903bef3b0b7be5e584ab81ebf28fa6f
lapatric <42653152+lapatric@users.noreply.github.com>
2025-09-01 12:18:14 +0200

    [DEV-4207] basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,181 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { useSearchParams } from "react-router-dom";
+import { InputField } from "./components/InputField";
+import { Button } from "./components/Button";
+
+type FormData = {
+  address: string;
+  message: string;
+  signature: string;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isLoading, setIsLoading] = useState(false);
+  const [verificationResult, setVerificationResult] = useState<boolean>();
+  const [error, setError] = useState<string | undefined>();
+
+  const [urlParams, setUrlParams] = useSearchParams();
+
+  useEffect(() => {
+    setUrlParams(new URLSearchParams());
+  }, []);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+    trigger,
+    setFocus,
+    watch,
+  } = useForm<FormData>({
+    mode: "onChange",
+    defaultValues: {
+      address: urlParams.get("address") || "",
+      message: urlParams.get("message") || "",
+      signature: urlParams.get("signature") || "",
+    },
+  });
+
+  watch(() => {
+    setError(undefined);
+    setVerificationResult(undefined);
+  });
+
+  async function onSubmit(data: FormData) {
+    setError(undefined);
+    setVerificationResult(undefined);
+
+    const { address, message, signature } = data;
+    const url = `https://dev.api.dfx.swiss/v1/auth/verifySignature?address=${encodeURIComponent(
+      address
+    )}&message=${encodeURIComponent(message)}&signature=${encodeURIComponent(
+      signature
+    )}`;
+    try {
+      const response = await fetch(url);
+      const responseJson = await response.json();
+      setVerificationResult(responseJson.isValid);
+    } catch (error: any) {
+      setError(error.toString());
+    }
+  }
+
+  const handleValidationAndFocus = async () => {
+    setIsLoading(true);
+    const isValid = await trigger();
+    if (!isValid) {
+      if (errors.address) {
+        setFocus("address");
+      } else if (errors.message) {
+        setFocus("message");
+      } else if (errors.signature) {
+        setFocus("signature");
+      }
+    }
+
+    await handleSubmit(onSubmit)();
+    setIsLoading(false);
+  };
 
   return (
-    <>
+    <div className="flex flex-col justify-start gap-8 p-2 pt-6 max-w-screen-sm">
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+        <div className="text-4xl font-extrabold text-slate-900 dark:text-slate-200 tracking-tight">
+          Verify a signed crypto message
+        </div>
+        <p className="mt-4 text-lg text-slate-700 dark:text-slate-400 leading-snug">
+          Enter the address, message, and signature to verify the signature.
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <form>
+        <div className="flex flex-col gap-4">
+          <InputField
+            id="address"
+            label="Address"
+            register={register}
+            errors={errors}
+          />
+          <InputField
+            id="message"
+            label="Message"
+            register={register}
+            errors={errors}
+            isTextArea
+          />
+          <InputField
+            id="signature"
+            label="Signature"
+            register={register}
+            errors={errors}
+          />
+
+          {error && (
+            <div className="bg-red-200 text-red-500 p-2 rounded-md">
+              {error}
+            </div>
+          )}
+
+          <Button
+            label={isLoading ? "VERIFYING..." : "VERIFY"}
+            onClick={handleValidationAndFocus}
+            disabled={isLoading}
+            isLoading={isLoading}
+            isGrayedOut={!isValid}
+          />
+
+          {verificationResult !== undefined && (
+            <div
+              className={`mt-2 p-2 rounded-md font-medium ${
+                verificationResult
+                  ? "bg-green-200 text-green-800"
+                  : "bg-red-200 text-red-500"
+              }`}
+            >
+              {verificationResult
+                ? "Signature is valid"
+                : "Signature is invalid"}
+            </div>
+          )}
+
+          <div className="mt-4 text-sm text-slate-500 dark:text-slate-400">
+            This website is operated and maintained as a non-profit project. The
+            model was{" "}
+            <a
+              href="https://www.verifybitcoinmessage.com"
+              target="_blank"
+              rel="noreferrer"
+              className="underline text-blue-500 hover:text-blue-600"
+            >
+              https://www.verifybitcoinmessage.com
+            </a>
+            . However, this website can check not only Bitcoin signatures but
+            also Ethereum, Monero and other blockchain signatures. If you have
+            any questions or suggestions, please contact{" "}
+            <a
+              href="mailto: verifycryptomessage@gmail.com"
+              className="underline text-blue-500 hover:text-blue-600"
+            >
+              verifycryptomessage@gmail.com
+            </a>
+            .
+          </div>
+
+          <div className="mt-4 text-sm text-slate-500 dark:text-slate-400">
+            Example Usage:{" "}
+            <a
+              href="https://verifycryptomessage.com/?address=1JWYmwtrqu2ttRDgtriFqivofWixCqRPRa&message=Hello%2C%20World!&signature=IHLrykoT8n7gF8M%2FC9pKVtRkxX446wx01kTcSqNmMBOOKFublbr5fj1moM0BwNrqoAdp7RMLr%2FBE%2BBM84jGNjOM%3D"
+              className="underline text-blue-500 hover:text-blue-600 break-all"
+            >
+              https://verifycryptomessage.com/?address=1JWY...RPRa&message=Hello%2C%20World!&signature=IHLr...M%3D
+            </a>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/InputField.tsx
@@ -0,0 +1,53 @@
+interface InputFieldProps {
+  id: string;
+  label: string;
+  register: any;
+  errors: any;
+  type?: string;
+  isTextArea?: boolean;
+}
+
+export function InputField({
+  id,
+  label,
+  register,
+  errors,
+  type = "text",
+  isTextArea = false,
+}: InputFieldProps): React.JSX.Element {
+  return (
+    <div className="relative">
+      {isTextArea ? (
+        <textarea
+          id="message"
+          {...register(id, { required: `${label} is required` })}
+          className={`block h-24 px-2.5 pb-2.5 pt-4 w-full text-sm dark:bg-[#242424] text-gray-900 rounded-lg border-[1.8px] ${
+            errors[id] ? "border-red-500" : "border-gray-300 dark:border-gray-600 focus:border-blue-600"
+          } appearance-none dark:text-white focus:outline-none focus:ring-0 peer`}
+          placeholder=" "
+          autoComplete="off"
+        />
+      ) : (
+        <input
+          type={type}
+          id={id}
+          {...register(id, { required: `${label} is required` })}
+          className={`block px-2.5 pb-2.5 pt-4 w-full text-sm dark:bg-[#242424] text-gray-900 rounded-lg border-[1.8px] ${
+            errors[id] ? "border-red-500" : "border-gray-300 dark:border-gray-600 focus:border-blue-600"
+          } appearance-none dark:text-white focus:outline-none focus:ring-0 peer`}
+          placeholder=" "
+          autoComplete="off"
+        />
+      )}
+      <div className={`absolute text-sm text-gray-500 dark:text-gray-400 ${errors[id] ? "peer-focus:text-red-500" : "peer-focus:text-blue-600"} duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-6 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1`}>
+        <label htmlFor={id}>{label}</label>
+        <div className="absolute h-[12px] -z-10 w-11/12 top-[3px] left-1 bg-white dark:bg-[#242424] rounded-xs"/>
+      </div>
+      {errors[id] && (
+        <p className="text-left pl-2.5 pt-1 text-red-500 text-sm">
+          {errors[id].message}
+        </p>
+      )}
+    </div>
+  );
+}
